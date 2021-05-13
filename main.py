916327b5630640b0cbd50cdbc37d824644036b13
kebabremover420 <spamemail123312@gmail.com>
2021-05-13 09:12:23 +0200

    benedek

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template, request, jsonify, redirect, url_for, g, session
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.run()
